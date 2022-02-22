0c66d3c5b3c6529778f31653424f4d1baadf5495
전창기 <wjs1724@naver.com>
2022-02-22 20:20:34 +0900

    SOVLE 22.02.22

---
+++ 22.02.22/jcg/Main_BJ_10824_네수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_10824_네수 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long A = Long.parseLong(st.nextToken()+st.nextToken());
+		long B = Long.parseLong(st.nextToken()+st.nextToken());
+		System.out.println(A+B);
+
+	}
+
+}
